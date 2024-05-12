327241fcae7137b124df79802445f2552ad064e3
amin <141229569+amynmhjwb24@users.noreply.github.com>
2024-05-12 13:08:51 +0330

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.23',
     settings: {
       optimizer: {
         enabled: true,
